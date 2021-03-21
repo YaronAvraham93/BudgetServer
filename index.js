c5dee8623e4b2e856e480140b523854bda7900c6
Yaron <yaronavraham600@gmail.com>
2021-03-21 20:58:42 +0200

    Deleted files

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./db/index");
+const connectDB = require("./db/db");
 const logger = require("./logger/logger");
 const userRouter = require("./routes/user-router");
 const transaction = require("./routes/transaction-router");
 const app = express();
 
 connectDB();
-app.use(cors());
+
 app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
@@ -17,6 +16,6 @@ app.get("/healthcheck", (req, res) => {
   return res.status(200).send("OK");
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => logger.log("info", `Server run on port ${PORT}`));
