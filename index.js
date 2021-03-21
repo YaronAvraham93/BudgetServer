b8f54894732ad3ff877bd26550923f0fd768004f
Yaron <yaronavraham600@gmail.com>
2021-03-21 19:44:03 +0200

    configure now

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const express=require('express');
-const cors = require('cors')
-const connectDB = require('./db/index')
-const logger = require('./logger/logger')
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/index");
+const logger = require("./logger/logger");
+const userRouter = require("./routes/user-router");
+const transaction = require("./routes/transaction-router");
+const app = express();
 
-connectDB()
+connectDB();
 app.use(cors());
-app.use(express.json({extended:true}));
-const userRouter = require('./routes/user-router')
-const transaction = require('./routes/transaction-router')
+app.use(express.json({ extended: true }));
 
-app.use('/api/transaction',transaction)
-app.use('/api/user',userRouter)
-app.get('/health-check',(req,res) =>{res.send('OK')})
+app.get("/", (req, res) => res.send("API Running"));
+app.use("/api/transaction", transaction);
+app.use("/api/user", userRouter);
+app.get("/health-check", (req, res) => {
+  return res.status(200).json("OK");
+});
 
-const PORT=process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT,()=> logger.log('info',`Server run on port ${PORT}`))
+app.listen(PORT, () => logger.log("info", `Server run on port ${PORT}`));
