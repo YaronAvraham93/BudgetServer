ff77409e53e0ccb558bdb9384fdb5c79def258b6
Yaron <yaronavraham600@gmail.com>
2021-03-21 20:02:02 +0200

    edit api

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express();
 
 connectDB();
 app.use(cors());
-app.use(express.json({ extended: true }));
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API Running"));
 app.use("/api/transaction", transaction);
 app.use("/api/user", userRouter);
-app.get("/health-check", (req, res) => {
-  return res.status(200).json("OK");
+app.get("/healthcheck", (req, res) => {
+  return res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3001;
